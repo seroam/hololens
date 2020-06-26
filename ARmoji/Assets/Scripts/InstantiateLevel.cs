b2206ea71d6b8ac78e585c2371a7e221274c0752
Severin Marti <goldcoin@goldcoin.ch>
2020-06-26 12:13:37 +0200

    Placement works

--- ARmoji/Assets/Scripts/InstantiateLevel.cs
+++ ARmoji/Assets/Scripts/InstantiateLevel.cs
@@ -1,23 +1,31 @@
-﻿using System.Collections;
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InstantiateLevel : MonoBehaviour
 {
     public GameObject lanePrefab;
-    private GameObject current;
-
+    public static GameObject current = null;
 
     // Start is called before the first frame update
     void Start()
     {
         current =  Instantiate(lanePrefab, transform);
 
+        current.GetComponent<LookAtCamera>().enabled = true;
+
+        Interactable lane = current.AddComponent<Interactable>();
+        lane.OnClick.AddListener(() => PlaceLane.Place());
+
        // Destroy(current);
     }
 
     public void Reset(){
         Destroy(current);
-        Start();
+        //current = Instantiate(lanePrefab, transform);
+        Transform currentTransform = current.transform;
+        current = Instantiate(lanePrefab, currentTransform.position, currentTransform.rotation, transform);
+        ;
 	}
 }
---
+++ ARmoji/Assets/Scripts/PlaceLane.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceLane : MonoBehaviour
+{
+
+    private static bool placing = true;
+    // Start is called before the first frame update
+    void Start() {
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Bit shift the index of the layer (10) to get a bit mask
+        int layerMask = 1 << LayerMask.NameToLayer("floor");
+
+        // Does the ray intersect any objects on layer 10?
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, Mathf.Infinity, layerMask)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.black);
+
+            Debug.Log($"Hit in position\t({hit.point.x}, {hit.point.y}, {hit.point.z})");
+            if (placing) InstantiateLevel.current.transform.position = hit.point;
+        }
+        else {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.blue);
+        }
+    }
+
+    public static void Place(){
+        placing = false;
+        InstantiateLevel.current.GetComponent<LookAtCamera>().enabled = false;
+	}
+}
